913ea8fca771321f39a9e315ab4d0673d59eb6fa
SentientMaverick <monglord_13@yahoo.com>
2018-07-10 16:19:30 +0100

    Commit 2

---
+++ src/Actio.Common/Events/UserCreated.cs
@@ -0,0 +1,18 @@
+namespace Actio.Common.Events
+{
+    public class UserCreated
+    {
+        public string Email{get;}
+        public string Name{get;}
+
+        protected UserCreated()
+        {
+            
+        }
+        public UserCreated(string email, string name)
+        {
+            Email=email;
+            Name=name;
+        }
+    }
+}
